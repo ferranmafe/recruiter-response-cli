7fdd370c21be3809e5b584b0bbf434456abce6f3
Ferran Martínez <ferran.mafe@gmail.com>
2023-05-27 22:47:05 +0200

    feat: Added working list and set commands

--- recruiter_response_cli/main.py
+++ recruiter_response_cli/main.py
@@ -1,21 +1,45 @@
 import typer
-from rich import Table
+from rich.table import Table
+import yaml
+import os
 
 app = typer.Typer()
+CONFIG_FILE_PATH = '~/.config/recruiter_response/config.yaml'
 
+if not os.path.exists(os.path.dirname(CONFIG_FILE_PATH)):
+    os.makedirs(os.path.dirname(CONFIG_FILE_PATH))
 
 @app.command("set")
 def set_response(
-    title: str = typer.Option(help="Title of the response snippet"),
-    description: str = typer.Option(help="Content of the response for the recruiter"),
+    key: str = typer.Option(help="Key to find the response snippet"),
+    response: str = typer.Option(help="Content of the response for the recruiter"),
     language: str = typer.Option(help="Language in which to store the response")
 ):
-    pass
+    responses_by_lang = {}
+    if os.path.exists(CONFIG_FILE_PATH):
+        with open(CONFIG_FILE_PATH) as f:
+            responses_by_lang = yaml.safe_load(f)
+
+    if language not in responses_by_lang:
+        responses_by_lang[language] = []
+
+    responses_by_lang[language].append(
+        {
+            "key": key,
+            "response": response
+        }
+    )
+    with open(CONFIG_FILE_PATH, 'w') as f:
+        yaml.dump(responses_by_lang, f)
 
 
 @app.command("list")
 def list_responses():
-    pass
+    responses_by_lang = {}
+    if os.path.exists(CONFIG_FILE_PATH):
+        with open(CONFIG_FILE_PATH) as f:
+            responses_by_lang = yaml.safe_load(f)
+    print(responses_by_lang)
 
 
 @app.command("get")
